f7f890797f0275eaf75ad1aef899f96c9f198144
Lema31 <luisema31@gmail.com>
2025-01-31 21:07:19 -0400

    fixing model

--- main.py
+++ main.py
@@ -17,37 +17,34 @@ train_path = 'phylum_train_dataset.csv'
 # read data
 train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
 train_y = train.pop('Phylums')
+train_y = tf.keras.utils.to_categorical(train_y, num_classes=13)
 
 
 # convert data to NumPy arrays
 train_X = train.values
-train_y = train_y.values
+
 
 
 # define sequential model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(60, activation='relu', input_shape=(train_X.shape[1],)),
-    tf.keras.layers.Dense(30, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(train_X.shape[1],), kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    #tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    #tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(13, activation='softmax')
 ])
 
 # compile model
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
 # train model
-history = model.fit(train_X, train_y,validation_split = 0.2, shuffle=True, epochs=1000)
+history = model.fit(train_X, train_y,batch_size = 4, shuffle=True, epochs=1000)
 model.save('Phylum_recognition_model_V1.h5')
 
-# evaluate model on test data
 
-plt.plot(history.history['loss'], label='Pérdida de entrenamiento')
-plt.plot(history.history['val_loss'], label='Pérdida de validación')
-plt.xlabel('Epochs')
-plt.ylabel('Pérdida')
-plt.legend()
-plt.show()
+
 
 
--- predict.py
+++ predict.py
@@ -6,8 +6,9 @@ PHYLUMS = ['Protozoarios', 'Poriferos', 'cnidarios', 'ctanoforos', 'platelmintos
            'acantocefalos', 'moluscos', 'anelidos', 'artropodos', 'equinodermos', 'cordados']
 
 model = tf.keras.models.load_model("Phylum_recognition_model_V1.h5")
-input_data = np.array([1, 1, 0, 0, 0, 0, 0])
+input_data = np.array([1,0,0.66,0,0,0,1])
 input_data = input_data.reshape(1, -1)
 prediction = model.predict(input_data)
 phylumIndex = np.argmax(prediction)
 print(PHYLUMS[phylumIndex])
+print(np.max(prediction))
